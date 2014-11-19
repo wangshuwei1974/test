52b48e5d9f8f6fe121e706c09267b1d17e531c4c
shuwei Wang <shuweiw@263.net>
2014-11-19 21:51:21 +0800

    add a shell for getopts for bash

---
+++ getoption.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo $*
+
+while getopts ":a:bcde" opt
+do 
+	case $opt in
+	a)
+	echo "option a is set:${OPTARG};in position ${OPTIND}"
+	;;
+	b)
+	echo "option b is set, in position ${OPTIND}"
+	;;
+	c)
+	echo "option c is set, in position ${OPTIND}"
+	;;
+	?)
+	echo "unrecognized option, error, exit"
+	;;
+	esac
+done
+echo "option index is :$OPTIND"
+
+shift $(( $OPTIND - 1 ))
+echo $0
+echo $*
+
